a0cb73c3c3341096e2f2d96978708e43afd4cd92
Jeremy Zilar <jeremyzilar@gmail.com>
2018-05-11 12:34:25 -0400

    adding in quote boxes

---
+++ assets/js/custom.js
@@ -0,0 +1,32 @@
+(function($){
+  jQuery(document).ready(function() {
+
+    // A minimal slideshow for the #quote-box
+    var currentIndex = 0,
+    items = $('#quote-frame .quote'),
+    itemAmt = items.length;
+    $('#quote-frame .quote:first-child').addClass('active');
+
+    function cycleItems() {
+      var item = $('#quote-frame .quote').eq(currentIndex);
+      items.removeClass('active');
+      item.addClass('active');
+    }
+    var autoSlide = setInterval(function() {
+      currentIndex += 1;
+      if (currentIndex > itemAmt - 1) {
+        currentIndex = 0;
+      }
+      cycleItems();
+    }, 4000);
+
+    // Determine which is the tallest of the .quote and set all the rest to that same height
+    var maxHeight = -1;
+    $('#quote-frame .quote').each(function() {
+      maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
+    });
+    console.log(maxHeight);
+    $('#quote-frame .paper').height(maxHeight + 30);
+
+  });
+})(jQuery);
--- index.md
+++ index.md
@@ -2,19 +2,15 @@
 # You don't need to edit this file, it's empty on purpose.
 # Edit theme's home layout instead if you wanna make some changes
 # See: https://jekyllrb.com/docs/themes/#overriding-theme-defaults
-layout: default
+layout: home
 ---
 
-# Hello!
+**Hello!** — We are working on building out this new site. Feel free to click around.
 
-We are working on building out this new site. Feel free to click around.
-
-### What will go on the homepage:
+**What will go on the homepage:**
 
 - Placeholder for CoE graphics and team photos
 - Latest tweets, blogs and articles
 - Announcements and success
 
-coming soon...
-
-:fox_face:
+more coming soon... :fox_face:
