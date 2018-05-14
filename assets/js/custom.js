9621e50dd0558e857a15139fe3bccad5bc9ce169
Jeremy Zilar <jeremyzilar@gmail.com>
2018-05-13 22:36:41 -0400

    adding a pop-up w/ cookie

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,28 @@
+function setCookie(key, value) {
+  var expires = new Date();
+  expires.setTime(expires.getTime() + (60 * (60 * 1000)) );
+  document.cookie = key + '=' + value + ';expires=' + expires.toUTCString() + "; path=/";
+}
+
+function getCookie(key) {
+  var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+  return keyValue ? keyValue[2] : null;
+}
+
 (function($){
   jQuery(document).ready(function() {
+
+    // set a cookie
+    if (getCookie('in_development') == 'true') {
+      $(".popup-frame").hide();
+    } else {
+      $(".popup-frame").show();
+      setCookie('in_development','true');
+    }
+
+    $( ".popup" ).click(function() {
+      $(".popup-frame").hide();
+    });
 
     // A minimal slideshow for the #quote-box
     var currentIndex = 0,
@@ -25,8 +48,8 @@
     $('#quote-frame .quote').each(function() {
       maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
     });
-    console.log(maxHeight);
     $('#quote-frame .paper').height(maxHeight);
 
   });
+
 })(jQuery);
