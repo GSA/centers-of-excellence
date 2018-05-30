be5a000c4e4a45c7b995ca444d67c8991dd806b5
Jeremy Zilar <jeremyzilar@gmail.com>
2018-05-30 14:42:43 -0400

    FIXING

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
-const gulp        = require('gulp');
-const imagemin    = require('gulp-imagemin');
+var gulp            = require('gulp'),
+    tap             = require('gulp-tap'),
+    responsive      = require('gulp-responsive');
 
-gulp.task('default', () =>
-  gulp.src('_images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'))
-);
+gulp.task('img', function () {
+  return gulp.src('/_images/**/*.{jpg,png,jpeg,gif,PNG,JPEG,JPG}')
+    .pipe(tap(function (file) {
+      console.log(file.path);
+      var uid = get_image_uid(file.path);
+      var format = get_image_format(file.path);
+      var dimensions = sizeOf(file.path);
+      fs.writeFile('dist/images/'+ uid +'.yml', get_image_data(uid, dimensions.width, dimensions.height, format));
+    }))
+
+});
+
+// Set watch as default task
+gulp.task('default', ['img']);
