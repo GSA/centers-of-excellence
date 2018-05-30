6c95dbfe0cc7583a61fe09e35eaabf2291bfdd36
Jeremy Zilar <jeremyzilar@gmail.com>
2018-05-30 13:32:37 -0400

    adding gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp        = require('gulp');
 const imagemin    = require('gulp-imagemin');
 
 gulp.task('default', () =>
-  gulp.src('images/*')
+  gulp.src('_images/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images'))
 );
